8f26cba0eefd0ea7ce9a9f959198a1849605719c
NateEbling <ebling.nathan@gmail.com>
2025-03-20 18:48:30 -0400

    Added nvim config files

---
+++ nvim/lua/nate/set.lua
@@ -0,0 +1,26 @@
+vim.opt.guicursor = "n-v-i-c:blinkon10-Cursor"
+
+--vim.opt.nu = true
+--vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.cursorline = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.modeline = true
+vim.opt.encoding = "utf-8"
+vim.opt.hlsearch = false
+vim.opt.history = 10000
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+vim.opt.ruler = true
+vim.opt.shiftround = true
+vim.opt.wrap = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+vim.o.splitright = true
