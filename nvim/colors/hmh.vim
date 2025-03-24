7d3eac0d79421d4fe61f81ce11f87f4ef6239b09
NateEbling <ebling.nathan@gmail.com>
2025-03-23 20:00:27 -0400

    Added custom statusline

--- nvim/colors/hmh.vim
+++ nvim/colors/hmh.vim
@@ -19,6 +19,7 @@ let s:col.grey         = ['#7f7f7f', 244]
 let s:col.blue         = ['#191970', 16]
 let s:col.green        = ['#6b8e23', 15]
 let s:col.red          = ['#ff0000', 196]
+let s:col.white        = ['#ffffff', 16]
 let s:col.none         = ['NONE', 'NONE']
 let s:underline        = 'underline'
 
@@ -76,6 +77,10 @@ call s:HL('Comment', 'grey', 'none')
 call s:HL('Keyword', 'dyellow', 'none')
 call s:HL('Statement', 'default', 'none')
 call s:HL('StatusLine', 'default', 'lightbg')
+call s:HL('LineNr', 'default', 'none')
+call s:HL('NonText', 'grey', 'none')
+call s:HL('MatchParen', 'default', 'grey')
+call s:HL('Todo', 'white', 'dyellow')
 call s:HL('Keyword', 'dyellow', 'none')
 
 if has('nvim')
--- nvim/lua/nate/set.lua
+++ nvim/lua/nate/set.lua
@@ -1,26 +1,43 @@
-vim.opt.guicursor = "n-v-i-c:blinkon10-Cursor"
+local o = vim.opt
 
---vim.opt.nu = true
---vim.opt.relativenumber = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.cursorline = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.smartcase = true
-vim.opt.ignorecase = true
-vim.opt.modeline = true
-vim.opt.encoding = "utf-8"
-vim.opt.hlsearch = false
-vim.opt.history = 10000
-vim.opt.incsearch = true
-vim.opt.termguicolors = true
-vim.opt.ruler = true
-vim.opt.shiftround = true
-vim.opt.wrap = true
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undofile = true
-vim.o.splitright = true
+o.guicursor = "n-v-i-c:blinkon10-Cursor"
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+o.cursorline = true
+o.autoindent = true
+o.smartindent = true
+o.smartcase = true
+o.ignorecase = true
+o.modeline = true
+o.encoding = "utf-8"
+o.hlsearch = false
+o.history = 10000
+o.incsearch = true
+o.termguicolors = true
+o.ruler = true
+o.shiftround = true
+o.wrap = true
+o.swapfile = false
+o.backup = false
+o.undofile = true
+o.splitright = true
+
+function Statusline()
+    local modified = vim.bo.modified and "*" or "-"
+    local version = vim.version()
+    local enc = vim.o.fenc ~= "" and vim.o.fenc or vim.o.enc
+    local status = string.format(
+        "-%s NVIM %s.%s.%s: %%t (%s) %%f %%= %s --",
+        modified,
+        version.major,
+        version.minor,
+        version.patch,
+        enc,
+        "%P"
+    )
+    return status
+end
+vim.o.statusline = "%!v:lua.Statusline()"
+vim.o.fillchars = "stl:-"
