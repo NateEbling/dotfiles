6801a146cbd98c98ded8390c09b23388dff4c432
NateEbling <ebling.nathan@gmail.com>
2025-04-06 19:06:24 -0400

    changed explorer command

--- nvim/lua/nate/remap.lua
+++ nvim/lua/nate/remap.lua
@@ -1,4 +1,4 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<C-x>", "", { noremap = true })
 vim.keymap.set("n", "<C-x><C-f>", ":lua FindFile()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-x>e", vim.cmd.Ex)
